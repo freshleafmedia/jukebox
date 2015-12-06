50561d83fc7d145b71db72ccbef21e2b696bf155
Joe Strong <joestrong@gmail.com>
2015-12-06 03:05:30 +0000

    Changed playlist to resolve_list in websocket server

--- websocketserver.js
+++ websocketserver.js
@@ -1,16 +1,18 @@
 var io = require('socket.io')(3000);
 var fs  = require("fs");
 
+var resolve_list = 'resolve_list';
+
 io.on('connection', function(socket){
     console.log('a user connected');
 
-    fs.readFile('playlist', function(err, f){
+    fs.readFile(resolve_list, function(err, f){
         var queue = f.toString().split('\n');
         socket.emit('queuelist', queue);
     });
 
     socket.on('addsong', function(data) {
-        fs.appendFile('playlist', data.id + "\n", function (err) {
+        fs.appendFile(resolve_list, data.id + "\n", function (err) {
             console.log('song added: ' + data.id);
             io.emit('newsong', data);
         });
