ac4c0d7d732b158e5dd5a6ed41035b6ed984d813
MoeBrowne <oliver.browne@uplyme.com>
2015-12-08 00:53:07 +0000

    Added calls to resolve a song when node receives a request

--- websocketserver.js
+++ websocketserver.js
@@ -19,26 +19,52 @@ function commitCache() {
     fs.writeFile('songcache.json', JSON.stringify(songCache));
 }
 
+function queueSong(song) {
+    io.emit('newsong', song);
+}
+
 io.on('connection', function(socket){
     console.log('a user connected');
 
     fs.readFile(resolve_list, function(err, f){
         var queue = f.toString().split('\n');
-	queue = queue.filter(function(item) {
-	    return item;
-	});
-	queue = queue.map(function(item) {
-	    return songCache[item] || { id: item, title: 'Unknown', thumbnail: '' };
-	});
+
+        queue = queue.filter(function(item) {
+            return item;
+        });
+
+        queue = queue.map(function(item) {
+            return songCache[item] || { id: item, title: 'Unknown', thumbnail: '' };
+        });
+
         socket.emit('queuelist', queue);
     });
 
     socket.on('addsong', function(song) {
-        fs.appendFile(resolve_list, song.id + "\n", function (err) {
-            console.log('song added: ' + song.id);
-	    songCache[song.id] = song;
-	    commitCache();
-            io.emit('newsong', song);
+
+        console.log('Resolving '+song.id);
+
+        // Check if we have already resolved this songs ID
+        if (typeof songCache[song.id] !== 'undefined' && typeof songCache[song.id]['URL'] !== 'undefined') {
+            queueSong(song);
+            return;
+        }
+
+        process.exec('./resolve.sh '+song.id, function (error, stdout, stderr) {
+            console.log('stderr: ' + stderr);
+            console.log('stdout: ' + stdout);
+
+            if (error !== null) {
+                console.log('exec error: ' + error);
+                return;
+            }
+
+            // The resolve.sh will return the URL
+            song['URL'] = stdout;
+            songCache[song.id] = song;
+
+            commitCache();
+            queueSong(song);
         });
     });
     socket.on('pause', function() {
