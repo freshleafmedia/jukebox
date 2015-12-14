203874e16fc64f9c78c1af85792531bbe24e5b1d
MoeBrowne <oliver.browne@uplyme.com>
2015-12-14 22:58:15 +0000

    Removed config file creation

--- install.sh
+++ install.sh
@@ -12,17 +12,3 @@ sudo apt-get install -y apache2 nodejs nodejs-legacy npm vlc
 # Install Youtube DL
 curl https://yt-dl.org/downloads/2015.12.05/youtube-dl -o /usr/local/bin/youtube-dl
 chmod a+rx /usr/local/bin/youtube-dl
-
-# Ask the user for the details
-read -e -p "Path to the config file: " -i "$HOME/.jukebox" CONFIG_PATH
-
-# Check if the config file already exists
-if [ -f "$CONFIG_PATH" ]; then
-	echo "The config file already exists, overwrite?"
-	select yn in "Yes" "No"; do
-		case $yn in
-			Yes ) echo -n > "$CONFIG_PATH"; break;;
-			No ) exit;;
-		esac
-	done
-fi
