1af2b01f65a03c8a53c6a9ae2f73c67de6246a28
Joe Strong <joe@freshleafmedia.co.uk>
2015-12-15 13:29:54 +0000

    Got service worker successfully working

--- worker.js
+++ worker.js
@@ -1,22 +1,25 @@
 self.addEventListener('install', function(event) {
-    console.log('installing v7');
+    console.log('Worker: installing v13');
     event.waitUntil(
         caches.open('offline').then(function(cache) {
-            return cache.add('offline.html');
+            fetch('/offline.html').then(function(response) {
+                return cache.put('offline.html', response);
+            });
         })
     );
 });
 
 self.addEventListener('activate', function(event) {
+    console.log('Worker: activating v13');
 });
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         fetch(event.request)
         .catch(function(e) {
-            caches.open('offline').then(function(cache){
-                return cache.match('offline.html');
-            });
+                console.log('Worker: fetch failed, responding with offline page...');
+                var offlinePage = caches.match('offline.html');
+                return offlinePage;
         })
     );
 });
