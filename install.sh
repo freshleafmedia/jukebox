292d597d4adb529d93cec0529d4e10b2a27c0d3d
MoeBrowne <oliver.browne@uplyme.com>
2015-12-19 17:35:18 +0000

    Added nvm to the installer

--- install.sh
+++ install.sh
@@ -9,6 +9,14 @@ echo "-------------------------------------------------"
 sudo apt-get update
 sudo apt-get install -y apache2 nodejs nodejs-legacy npm vlc
 
+# Install NVM
+curl https://raw.githubusercontent.com/creationix/nvm/v0.11.1/install.sh | bash
+source ~/.nvm/nvm.sh
+
+nvm install v5.0
+nvm use v5.0
+nvm alias default v5.0
+
 # Install Youtube DL
 curl https://yt-dl.org/downloads/2015.12.05/youtube-dl -o /usr/local/bin/youtube-dl
 chmod a+rx /usr/local/bin/youtube-dl
