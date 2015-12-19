6deee8002e4aa96cb3ddaad0c2bd8a1636ee7f28
MoeBrowne <oliver.browne@uplyme.com>
2015-12-19 17:55:18 +0000

    Set the youtube-dl to be run as root

--- install.sh
+++ install.sh
@@ -16,5 +16,5 @@ source ~/.nvm/nvm.sh
 nvm install v5.0
 
 # Install Youtube DL
-curl https://yt-dl.org/downloads/2015.12.05/youtube-dl -o /usr/local/bin/youtube-dl
-chmod a+rx /usr/local/bin/youtube-dl
+sudo curl https://yt-dl.org/downloads/2015.12.05/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
