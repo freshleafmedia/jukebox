ecead099f55571c5e554e8944e3f8dfee7837fac
Joe Strong <joe@freshleafmedia.co.uk>
2016-01-26 13:45:40 +0000

    Added new soundbite

--- js/source/config/soundbites.js
+++ js/source/config/soundbites.js
@@ -2,6 +2,7 @@ export default {
     sounds: [
         'YLgKPEhyg3k', // Jam Sandwich
         'cdEQmpVIE4A', // Disappointed
-        'kD_xWrhZ8yI' // That's what she said
+        'kD_xWrhZ8yI', // That's what she said
+        '3KVEe6UVpYw' // Kids 'Yay'
     ]
 };
