92a064e75b670ffa37778793a071fdecd499c3dc
Joe Strong <joe@freshleafmedia.co.uk>
2016-06-27 12:28:09 +0000

    Added Wahoo! soundbit

--- js/source/config/soundbites.js
+++ js/source/config/soundbites.js
@@ -4,6 +4,8 @@ export default {
         /* 2 */ {id: 'cdEQmpVIE4A', name: 'Disappointed'},
         /* 3 */ {id: 'kD_xWrhZ8yI', name: "That's what she said"},
         /* 4 */ {id: '3KVEe6UVpYw', name: "Kids 'Yay'"},
-        /* 5 */ {id: '5Q_2nrp9OTg', name: "Booo!"}
+        /* 5 */ {id: '5Q_2nrp9OTg', name: "Booo!"},
+        /* 6 */ {id: 'tt6BI1-VyX8', name: "Yeah Boiii"},
+        /* 7 */ {id: '5M-9wAmdMWI', name: "Whoa!"}
     ]
 };
