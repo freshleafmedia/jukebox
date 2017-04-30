76e106c924a7f88bc9918f3ebac11d4c9762eb7c
MoeBrowne <oliver.browne@uplyme.com>
2017-04-30 19:24:11 +0100

    Removed calls to sudo and prevented the script from running if not root

--- install.sh
+++ install.sh
@@ -5,9 +5,14 @@ echo "                JUKEBOX INSTALLER"
 echo "                      v0.1.0"
 echo "-------------------------------------------------"
 
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
+fi
+
 # Install APT packages
-sudo apt-get update
-sudo apt-get install -y apache2 vlc
+apt-get update
+apt-get install -y apache2 vlc
 
 # Install NVM
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
@@ -20,5 +25,5 @@ nvm install v7
 npm install
 
 # Install Youtube DL
-sudo curl https://yt-dl.org/latest/youtube-dl -o /usr/local/bin/youtube-dl
-sudo chmod a+rx /usr/local/bin/youtube-dl
+curl https://yt-dl.org/latest/youtube-dl -o /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
