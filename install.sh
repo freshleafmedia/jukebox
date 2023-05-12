66d0615d18dcc0274344eef2c83dc7cbc357fe87
Freshleaf Media <10062339+freshleafmedia@users.noreply.github.com>
2023-05-12 18:28:22 +0100

    Adds conditional install for Yt-dlp

--- install.sh
+++ install.sh
@@ -17,7 +17,16 @@ apt-get update
 apt-get install -y vlc ffmpeg php8.2-cli php8.2-sqlite php8.2-curl php8.2-xml git unzip
 
 # Install Yt-dlp
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux -o /usr/local/bin/yt-dlp
+if [[ "$(dpkg --print-architecture)" == "amd64" ]]; then
+    curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux -o /usr/local/bin/yt-dlp
+elif [[ "$(dpkg --print-architecture)" == "armhf" ]]; then
+    curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux_armv7l -o /usr/local/bin/yt-dlp
+else
+    echo "Error: unknown arch."
+    echo "Please install Yt-dlp manually: https://github.com/yt-dlp/yt-dlp/releases/latest/"
+    exit 1
+fi
+
 chmod a+rx /usr/local/bin/yt-dlp
 
 # Install Composer
