55e4995e5fdf0b91a9ece72075738b4dbdbb54ec
Freshleaf Media <10062339+freshleafmedia@users.noreply.github.com>
2023-05-12 18:28:04 +0100

    Adds additional PHP modules

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ fi
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:ondrej/php
 apt-get update
-apt-get install -y vlc ffmpeg php8.2 php8.2-sqlite git unzip
+apt-get install -y vlc ffmpeg php8.2-cli php8.2-sqlite php8.2-curl php8.2-xml git unzip
 
 # Install Yt-dlp
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux -o /usr/local/bin/yt-dlp
