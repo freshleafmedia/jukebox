baadaacb5372b68cab82377f48ce29a77c10a136
Freshleaf Media <10062339+freshleafmedia@users.noreply.github.com>
2023-05-12 17:39:13 +0100

    Adds Composer to install script

--- install.sh
+++ install.sh
@@ -14,8 +14,12 @@ fi
 apt-get install -y software-properties-common
 add-apt-repository ppa:ondrej/php
 apt-get update
-apt-get install -y vlc ffmpeg php8.2 php8.2-sqlite
+apt-get install -y vlc ffmpeg php8.2 php8.2-sqlite git unzip
 
 # Install Yt-dlp
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux -o /usr/local/bin/yt-dlp
 chmod a+rx /usr/local/bin/yt-dlp
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | /usr/bin/php -- --install-dir=/usr/bin --filename=composer
+chmod 0755 /usr/bin/composer
