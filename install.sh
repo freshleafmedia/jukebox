06a4f030f491cca9d19be25c5b56fe53000f8332
Freshleaf Media <10062339+freshleafmedia@users.noreply.github.com>
2023-05-12 18:56:23 +0100

    Updats install script and service permissions

--- install.sh
+++ install.sh
@@ -7,36 +7,31 @@ echo "                JUKEBOX INSTALLER"
 echo "                      v2.0.0"
 echo "-------------------------------------------------"
 
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root"
-    exit 1
-fi
-
 # Install APT packages
-apt-get install -y software-properties-common
-add-apt-repository -y ppa:ondrej/php
-apt-get update
-apt-get install -y vlc ffmpeg php8.2-cli php8.2-sqlite php8.2-curl php8.2-xml php8.2-mbstring git unzip
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install -y vlc ffmpeg php8.2-cli php8.2-sqlite php8.2-curl php8.2-xml php8.2-mbstring git unzip
 
 # Install Yt-dlp
 if [[ ! -f /usr/local/bin/yt-dlp ]]; then
     if [[ "$(dpkg --print-architecture)" == "amd64" ]]; then
-        curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux -o /usr/local/bin/yt-dlp
+        sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux -o /usr/local/bin/yt-dlp
     elif [[ "$(dpkg --print-architecture)" == "armhf" ]]; then
-        curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux_armv7l -o /usr/local/bin/yt-dlp
+        sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux_armv7l -o /usr/local/bin/yt-dlp
     else
         echo "Error: unknown arch."
         echo "Please install Yt-dlp manually: https://github.com/yt-dlp/yt-dlp/releases/latest/"
         exit 1
     fi
 
-    chmod a+rx /usr/local/bin/yt-dlp
+    sudo chmod a+rx /usr/local/bin/yt-dlp
 fi
 
 
 # Install Composer
-curl -sS https://getcomposer.org/installer | /usr/bin/php -- --install-dir=/usr/bin --filename=composer
-chmod 0755 /usr/bin/composer
+curl -sS https://getcomposer.org/installer | sudo /usr/bin/php -- --install-dir=/usr/bin --filename=composer
+sudo chmod 0755 /usr/bin/composer
 
 # Install dependencies
 if [[ ! -d "${__DIR__}/vendor" ]]; then
@@ -57,21 +52,33 @@ if [[ ! -f "${__DIR__}/database/database.sqlite" ]]; then
 fi
 
 # Install services
-sed "s|{{ PWD }}|${__DIR__}|g" "${__DIR__}/stubs/systemctl/jukebox-queue-worker.service" > /etc/systemd/system/jukebox-queue-worker.service
-sed "s|{{ PWD }}|${__DIR__}|g" "${__DIR__}/stubs/systemctl/jukebox-server-artisan.service" > /etc/systemd/system/jukebox-server-artisan.service
-sed "s|{{ PWD }}|${__DIR__}|g" "${__DIR__}/stubs/systemctl/jukebox-server-octane.service" > /etc/systemd/system/jukebox-server-octane.service
-sed "s|{{ PWD }}|${__DIR__}|g" "${__DIR__}/stubs/systemctl/jukebox-vlc-player.service" > /etc/systemd/system/jukebox-vlc-player.service
-chmod 644 /etc/systemd/system/jukebox-*.service
-systemctl daemon-reload
+sudo sed -i "s|{{ PWD }}|${__DIR__}|g" "${__DIR__}/stubs/systemctl/jukebox-queue-worker.service"
+sudo sed "s|{{ UID }}|${UID}|g" "${__DIR__}/stubs/systemctl/jukebox-queue-worker.service"
+sudo cp "${__DIR__}/stubs/systemctl/jukebox-queue-worker.service" /etc/systemd/system/
+
+sudo sed -i "s|{{ PWD }}|${__DIR__}|g" "${__DIR__}/stubs/systemctl/jukebox-server-artisan.service"
+sudo sed "s|{{ UID }}|${UID}|g" "${__DIR__}/stubs/systemctl/jukebox-server-artisan.service"
+sudo cp "${__DIR__}/stubs/systemctl/jukebox-server-artisan.service" /etc/systemd/system/
+
+sudo sed -i "s|{{ PWD }}|${__DIR__}|g" "${__DIR__}/stubs/systemctl/jukebox-server-octane.service"
+sudo sed "s|{{ UID }}|${UID}|g" "${__DIR__}/stubs/systemctl/jukebox-server-octane.service"
+sudo cp "${__DIR__}/stubs/systemctl/jukebox-server-octane.service" /etc/systemd/system/
+
+sudo sed -i "s|{{ PWD }}|${__DIR__}|g" "${__DIR__}/stubs/systemctl/jukebox-vlc-player.service"
+sudo sed "s|{{ UID }}|${UID}|g" "${__DIR__}/stubs/systemctl/jukebox-vlc-player.service"
+sudo cp "${__DIR__}/stubs/systemctl/jukebox-vlc-player.service" /etc/systemd/system/
+
+sudo chmod 644 /etc/systemd/system/jukebox-*.service
+sudo systemctl daemon-reload
 
 # Enable services
-systemctl enable jukebox-queue-worker
-systemctl enable jukebox-server-artisan
-#systemctl enable jukebox-server-octane
-systemctl enable jukebox-vlc-player
+sudo systemctl enable jukebox-queue-worker
+sudo systemctl enable jukebox-server-artisan
+#sudo systemctl enable jukebox-server-octane
+sudo systemctl enable jukebox-vlc-player
 
 # Start services
-systemctl start jukebox-queue-worker
-systemctl start jukebox-server-artisan
-#systemctl start jukebox-server-octane
-systemctl start jukebox-vlc-player
+sudo systemctl start jukebox-queue-worker
+sudo systemctl start jukebox-server-artisan
+#sudo systemctl start jukebox-server-octane
+sudo systemctl start jukebox-vlc-player
