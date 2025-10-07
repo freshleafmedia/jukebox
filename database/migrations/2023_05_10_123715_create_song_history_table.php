88a508ba60a0da7fecc139afe0e151422e7684b9
Freshleaf Media <10062339+freshleafmedia@users.noreply.github.com>
2025-10-07 20:30:08 +0100

    Adds additional relation index

--- database/migrations/2023_05_10_123715_create_song_history_table.php
+++ database/migrations/2023_05_10_123715_create_song_history_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('song_history', function (Blueprint $table) {
             $table->id()->index();
-            $table->foreignId('song_id')->constrained('songs');
+            $table->foreignId('song_id')->index()->constrained('songs');
             $table->text('played_by');
             $table->timestamps();
         });
