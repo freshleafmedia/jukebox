7c8d9f1350d5d769e8bcbcaf5c6f7cfb645c5d1f
Freshleaf Digital <10062339+freshleafmedia@users.noreply.github.com>
2026-08-31 19:13:55 +0100

    Updates style

--- src/routes/sse.php
+++ src/routes/sse.php
@@ -11,17 +11,6 @@ while (ob_get_level() > 0) {
     ob_end_clean();
 }
 
-function sendSseData(string $data): void
-{
-    foreach (explode("\n", $data) as $line) {
-        echo 'data: ' . $line . "\n";
-    }
-
-    echo "\n";
-
-    flush();
-}
-
 $signatureStatement = Db::connect()->prepare('SELECT MAX(updated_at) FROM songs WHERE queued_by IS NOT NULL');
 $activeSongStatement = Db::connect()->prepare('SELECT id, duration FROM songs WHERE queued_by IS NOT NULL AND state IN (?, ?) ORDER BY sort LIMIT 1');
 
@@ -34,13 +23,13 @@ while (!connection_aborted()) {
     $signatureStatement->execute();
     $signature = $signatureStatement->fetchColumn();
 
-    $partials = '';
+    $content = '';
 
     if ($signature !== $lastSignature) {
         $lastSignature = $signature;
 
-        $partials .= '<hx-partial hx-target="#mediaControls">' . renderView(__DIR__ . '/../views/media-controls.php') . '</hx-partial>' . "\n"
-            . '<hx-partial hx-target="#queueContainer">' . renderView(__DIR__ . '/../views/queue-list.php') . '</hx-partial>' . "\n";
+        $content .= '<hx-partial hx-target="#mediaControls">' . renderView(__DIR__ . '/../views/media-controls.php') . '</hx-partial>' . "\n";
+        $content .= '<hx-partial hx-target="#queueContainer">' . renderView(__DIR__ . '/../views/queue-list.php') . '</hx-partial>' . "\n";
     }
 
     $activeSongStatement->execute([SongState::PLAYING->value, SongState::PAUSED->value]);
@@ -52,15 +41,21 @@ while (!connection_aborted()) {
         if ($position !== null && $position !== $lastPosition) {
             $lastPosition = $position;
 
-            $partials .= '<hx-partial hx-target="#playbackProgress" hx-swap="outerHTML"><progress id="playbackProgress" max="' . $activeSong['duration'] . '" value="' . $position . '"></progress></hx-partial>' . "\n";
+            $content .= '<hx-partial hx-target="#playbackProgress" hx-swap="outerHTML"><progress id="playbackProgress" max="' . $activeSong['duration'] . '" value="' . $position . '"></progress></hx-partial>' . "\n";
         }
     }
 
-    if ($partials !== '') {
-        $partials .= '<hx-partial hx-target="#debugUpdateTime">' . renderView(__DIR__ . '/../views/debug-update-time.php', ['updateTime' => microtime(true) - $loopStartedAt]) . '</hx-partial>';
+    if ($content !== '') {
+        $content .= '<hx-partial hx-target="#debugUpdateTime">' . renderView(__DIR__ . '/../views/debug-update-time.php', ['updateTime' => microtime(true) - $loopStartedAt]) . '</hx-partial>';
     }
 
-    sendSseData($partials);
+    foreach (explode("\n", $content) as $line) {
+        echo 'data: ' . $line . "\n";
+    }
+
+    echo "\n";
+
+    flush();
 
     usleep(100_000);
 }
